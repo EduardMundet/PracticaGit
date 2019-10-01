6eb0cc430e9dc26f9b0b0253d14a483a091b3f89
Eduard.Mundet <41579168K@institutmontilivi.cat>
2019-10-01 16:02:49 +0200

    base del programa

---
+++ Llista-jocs/Llista-jocs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Llista_jocs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escull un joc:");
+            switch (Console.ReadLine())
+            {
+                default:
+                    break;
+            }
+        }
+    }
+}
